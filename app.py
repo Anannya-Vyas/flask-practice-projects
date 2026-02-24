dfcd9f5cbb23cd2ee87e49a9f9267f1bececc234
Anannya-Vyas <beyondanannyavyas@gmail.com>
2026-02-24 11:22:57 +0530

    second day

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def render_template_index():  
      return render_template('index.html', name='Flask')
+     
+
+
+@app.route('/about')
+def render_template_about():
+     return render_template('about.html', name='Flask')                    
 
 
 
